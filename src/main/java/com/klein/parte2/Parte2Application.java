03747c7f400d78e0619119fdd9a4440ef0f664dc
roberto klein <robertoluizklein@gmail.com>
2018-05-17 14:48:23 -0300

    subindo a aplicacao pela primeira ves

---
+++ src/main/java/com/klein/parte2/Parte2Application.java
@@ -0,0 +1,12 @@
+package com.klein.parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parte2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parte2Application.class, args);
+	}
+}
